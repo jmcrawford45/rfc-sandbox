b4ab1861e98c5f0fea34049962e5082a738de7ec
Jared Crawford <jmcrawford45@gmail.com>
2022-12-12 11:05:15 -0600

    bloom filter

---
+++ tests/test_bloom_filter.py
@@ -0,0 +1,9 @@
+from bloom_filter import BloomFilter
+
+def test_filter_basic():
+	bloom = BloomFilter()
+	for i in range(100):
+		if i % 2 == 0:
+			bloom.add(str(i).encode())
+	assert not bloom.contains(b"1")
+	assert bloom.contains(b"2")
